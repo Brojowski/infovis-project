314a741e4567dd174d8aec1360c5fd025ac6d091
Brojowski <alex.gajowski@gmail.com>
2020-04-29 14:48:58 -0400

    use vue methods instead of javascript ones

--- src/components/FilmVis.vue
+++ src/components/FilmVis.vue
@@ -41,56 +41,83 @@ import * as d3 from 'd3';
 export default {
   name: 'FilmVis',
   mounted: function() {
-    let svgWidth = 1500;
-    let svgHeight = 1000;
+    this.vis3 = d3.select('#vis3')
+        .attr('width', this.svgWidth)
+        .attr('height', this.svgHeight);
 
-    let xProp = "year"
-    let yProp = "popularity"  
-    let colorProp = "length"
-
-    let vis3 = d3.select('#vis3')
-        .attr('width', svgWidth)
-        .attr('height', svgHeight);
-
-    let dataGroup = vis3.append('g')
+    this.dataGroup = this.vis3.append('g')
         .attr('class', 'films data-group');
 
-    let _this = this;
-    console.log((_this.categories))
-    function renderFilms(data) {
-      //console.log(data)
+    //console.log("a1-film")
+    d3.csv('data/a1-film.csv', this.type).then(this.onDataLoaded)
+
+    console.log((this.categories))
+  },
+  data: () => {
+    return {
+      data: [],
+      svgWidth: 750,
+      svgHeight: 500,
+      xProp: "year",
+      yProp: "popularity"  ,
+      colorProp: "length",
+      vis3: null,
+      dataGroup: null,
+      categories: new Set(),
+      focused: {
+         year: "",
+         length: "",
+         title: "Hover over a movie to see info",
+         subject: "",
+         actor: "",
+         actress: "",
+         director: "",
+         popularity: "",
+         awards: "",
+      }
+    };
+  },
+  methods: {
+    onDataLoaded: function(data) {
+      console.log("data did load");
+      console.log(data[0]);
+      this.data = data;
+      this.renderFilms()
+    },
+    renderFilms: function() {
+        let data = this.data;
+        let _this = this;
 
         let xScale = d3.scaleLinear()
-            .domain(d3.extent(data, (d) => d[xProp]))
-            .range([5, svgWidth-5])
+            .domain(d3.extent(data, (d) => d[this.xProp]))
+            .range([5, this.svgWidth-5])
         let yScale = d3.scaleLinear()
-            .domain(d3.extent(data, (d) => d[yProp]))
-            .range([svgHeight-5, 5])
+            .domain(d3.extent(data, (d) => d[this.yProp]))
+            .range([this.svgHeight-5, 5])
         let alphaScale = d3.scaleLinear()
-            .domain(d3.extent(data, (d) => d[colorProp]))
+            .domain(d3.extent(data, (d) => d[this.colorProp]))
             .range([.1, 1])
         let colorScale = d3.scaleOrdinal()
-            .domain(_this.categories)
+            .domain(this.categories)
             .range(d3.schemeCategory10)
         console.log(colorScale)
 
-        let circles = dataGroup.selectAll('circle').data(data);
+        let circles = this.dataGroup.selectAll('circle').data(data);
 
         circles.enter().append('circle')
-            .attr('cx', (d) => xScale(d[xProp]))
-            .attr('cy', (d) => yScale(d[yProp]))
-            .attr('r', 5)
+            .attr('cx', (d) => xScale(d[this.xProp]))
+            .attr('cy', (d) => yScale(d[this.yProp]))
+            .attr('r', 2)
             .style('fill', (d) => {
-              return d3.color(colorScale(d.subject)).copy({ opacity: alphaScale(d[colorProp]) })
+              return d3.color(colorScale(d.subject)).copy({ opacity: alphaScale(d[this.colorProp]) })
             })
             .on('mouseover', function(d) {
               _this.focused = d;
             })
 
         circles.exit().remove();
-    }
-    
-    function type(d) {
+    },
+    type: function(d) {
         /*
          * year         INT
          * length       INT
@@ -106,28 +133,9 @@ export default {
         d.year = +d.year;
         d.length = +d.length;
         d.popularity = +d.popularity;
-        _this.categories.add(d.subject)
+        this.categories.add(d.subject)
         return d;
     }
-
-    //console.log("a1-film")
-    d3.csv('data/a1-film.csv', type).then(renderFilms)
-  },
-  data: () => {
-    return {
-      categories: new Set(),
-      focused: {
-         year: "",
-         length: "",
-         title: "Hover over a movie to see info",
-         subject: "",
-         actor: "",
-         actress: "",
-         director: "",
-         popularity: "",
-         awards: "",
-      }
-    };
   }
 }
 </script>
